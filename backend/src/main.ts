13c86c16fe572fc00a360fb7d150566fa950e1c9
Vyacheslav <VyacheslavTroynov@gmail.com>
2023-09-16 23:01:58 +0300

    peparing for deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,5 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3003);
+  app.enableCors({
+    // origin: config.get('CORS_ORIGIN'),
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
 }
 bootstrap();
